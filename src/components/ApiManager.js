398917e5707dfcd036d10e27559de87511e70632
Chris Abbondandelo <abbondcj@gmail.com>
2022-10-19 17:37:44 -0500

    bug fixes

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -5,6 +5,6 @@ export const states = database + "states"
 export const rounds = database + "rounds"
 export const favorite_courses = database + "favoriteCourses"
 export const gc_header = {
-    'X-RapidAPI-Key': '(*** INSERT RAPID API KEY HERE!!! ****)',
+    'X-RapidAPI-Key': '4a7a12f70bmsh354c4e76be8cdf6p1b69d3jsnf8c18f063789',
     'X-RapidAPI-Host': 'golf-course-finder.p.rapidapi.com'
 }
--- src/components/play/CourseSearch.jsx
+++ src/components/play/CourseSearch.jsx
@@ -93,27 +93,23 @@ export const CourseSearch = () => {
     clearSuggestions,
   } = usePlacesAutocomplete({
     requestOptions: {
-      /* Define search scope here */
     },
     debounce: 300,
   });
   
 
   const handleInput = (e) => {
-    // Update the keyword of the input element
     setValue(e.target.value);
   };
 
   const handleSelect =
     ({ description }) =>
     () => {
-      // When user selects a place, we can replace the keyword without request data from API
-      // by setting the second parameter to "false"
+     
       setValue(description, false);
       setSelectedLocation(description)
       clearSuggestions();
 
-      // Get latitude and longitude via utility functions
       getGeocode({ address: description }).then((results) => {
         const { lat, lng } = getLatLng(results[0]);
         retrieveCourses(lat.toString(), lng.toString())
